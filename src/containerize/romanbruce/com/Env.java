5a8813ba66ec3c1ade57e308e3f179a6d9bd3efc
Roman <71509815+romanwbruce@users.noreply.github.com>
2022-05-17 20:36:54 -0400

    rmv
    rmv

--- src/containerize/romanbruce/com/Env.java
+++ src/containerize/romanbruce/com/Env.java
@@ -2,5 +2,5 @@ package containerize.romanbruce.com;
 
 public class Env {
 
-	public static String url = "mongodb://hedgs:2646msrb@cluster0-shard-00-00.l17ml.mongodb.net:27017,cluster0-shard-00-01.l17ml.mongodb.net:27017,cluster0-shard-00-02.l17ml.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-2t3ga3-shard-0&authSource=admin&retryWrites=true&w=majority";
+	public static String url = "-----";
 }
